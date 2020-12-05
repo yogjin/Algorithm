3e654a7deef7e34c93f2fe30c27cdba1a97341e2
repl.it user <replituser@example.com>
2020-12-05 10:58:12 +0000

    StringBuilder 기초

---
+++ BOJ/10926.java
@@ -0,0 +1,18 @@
+/* 
+ * Author: Park Yeong-Jin
+ * Date: 2020.12.05
+ * email: youngjin988@gmail.com
+ * 
+ * https://github.com/yogjin/Algorithm
+ */
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Main {
+	public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.print(sb.append("??!"));
+    }
+}
