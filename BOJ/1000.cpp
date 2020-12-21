13bbde7e1d60164cc1679d6999cc6695aebca6c9
yogjin <youngjin988@gmail.com>
2020-12-21 14:58:06 +0000

    c++연습

---
+++ BOJ/1000.cpp
@@ -0,0 +1,14 @@
+/* 
+ * Author: Park Yeong-Jin
+ * Date: 2020.12.21
+ * email: youngjin988@gmail.com
+ * 
+ * https://github.com/yogjin/Algorithm
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+}
