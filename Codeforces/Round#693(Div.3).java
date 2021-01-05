d430a3ff895ad8f05a6a42f12f7e07cae558ade1
yogjin <youngjin988@gmail.com>
2021-01-05 14:45:27 +0000

    dp로 풀어보자..

--- Codeforces/Round#693(Div.3).java
+++ Codeforces/Round#693(Div.3).java
@@ -38,3 +38,39 @@ public class Main {
         System.out.print(sb);
     }
 }
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Main {
+	public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int t = Integer.parseInt(br.readLine());//t 테스트케이스 수
+        
+        for(int i = 0; i < t; i++){
+          int n = Integer.parseInt(br.readLine());//배열크기
+          st = new StringTokenizer(br.readLine());
+          int[] arr = new int[n+1];
+          boolean[] b = new boolean[n+1];
+          for(int j = 1; j <= n; j++){
+            arr[j] = Integer.parseInt(st.nextToken());//수 넣기
+          }
+          long max = 0;
+          long score = 0;
+          for(int k = 1; k <= n; k++){
+            score = 0;
+            int index = k;//시작 인덱스
+            while(index <= n && !b[index]){//인덱스가 베열크기를 넘어가지 않을정도로만
+              b[index] = true;
+              score += arr[index];
+              index += arr[index];
+            }
+            max = Math.max(max,score);
+          }
+          sb.append(max).append("\n");
+        }
+        System.out.print(sb);
+  }
+}
