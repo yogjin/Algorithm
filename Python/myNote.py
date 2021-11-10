24f50b686fd72d64ea3ce0345fa5e734dcf12a29
yogjin <youngjin988@gmail.com>
2021-11-10 15:15:27 +0000

    sort된 list에서 bisect를 활용해 값이 특정 범위에 속하는 원소의 개수 구하기

--- Python/myNote.py
+++ Python/myNote.py
@@ -42,3 +42,23 @@ input:
 board:
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 '''
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
+# array는 Sorting된 상태여야 한다!
+def count_by_range(array, left_value, right_value):
+    right_index = bisect_right(array, right_value)
+    left_index = bisect_left(array, left_value)
+    return right_index - left_index
+
+n, x = map(int, input().split()) # 데이터의 개수 N, 찾고자 하는 값 x 입력 받기
+array = list(map(int, input().split())) # 전체 데이터 입력 받기
+
+# 값이 [x, x] 범위에 있는 데이터의 개수 계산
+count = count_by_range(array, x, x)
+
+# 값이 x인 원소가 존재하지 않는다면
+if count == 0:
+    print(-1)
+# 값이 x인 원소가 존재한다면
+else:
+    print(count)
